6a567a479850d8c5195043a47d4baaacf66ad0f7
mroliveeiraa <133261097+mroliveeiraa@users.noreply.github.com>
2023-11-13 11:13:45 -0300

    Atualizar o testeJava.java

--- testeJava.java
+++ testeJava.java
@@ -1,16 +1,17 @@
 from unittest.mock import patch
-from src import post_ativacao_contratos
+from lambda_function import lambda_handler
 
 def test_lambda_handler():
-    with patch('src.post_ativacao_contratos.post_cuca_ativacao_contratos') as mock_post:
+    # Configurar o ambiente de teste com um mock para post_cuca_ativacao_contratos
+    with patch('lambda_function.post_ativacao_contratos.post_cuca_ativacao_contratos') as mock_post:
         # Configurar o retorno simulado da função post_cuca_ativacao_contratos
         mock_post.return_value = "Mocked Response"
 
-        # Chamar a função lambda_handler
+        # Chamar a função lambda_handler com argumentos fictícios
         result = lambda_handler({}, {})
 
-        # Verificar se a função post_cuca_ativacao_contratos foi chamada
+        # Verificar se a função post_cuca_ativacao_contratos foi chamada exatamente uma vez
         mock_post.assert_called_once()
 
-        # Verificar se o resultado da função lambda_handler é o esperado
+        # Verificar se o resultado da função lambda_handler é o esperado ("Mocked Response")
         assert result == "Mocked Response"
